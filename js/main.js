9b3bedb07221f357c556bb57224a9826a3621f6d
unknown <muhammedalaa2213@gmail.com>
2022-09-21 18:57:15 +0200

    a

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const title = document.querySelector(".WebSite").innerHTML;
+const texts = ["Welcome in our site", "BFCAI", "Let's Study !"];
+    let counter = 0;
+    let index = 0;
+    let text = "";
+    let size = "";
+(function type() {
+    
+    if (counter === text.length) {
+      counter = 0;
+    }
+    text = texts[counter];
+    size = text.slice(0, index++);
+    document.querySelector(".TypeWrite").textContent = size;
+    if (size === text.length) {
+      counter++;
+      index = 0;
+    }
+    setTimeout(type, 100);
+})();
